951dc36fba4951d376802d1fad694996357de8c6
Diya <pdiya2945@gmail.com>
2025-05-09 19:17:10 +0530

    changes in urls

--- chai/urls.py
+++ chai/urls.py
@@ -3,7 +3,7 @@ from .import views
 
 urlpatterns = [
     path('',views.all_chai,name='all_chai'),
-    path('temp/',views.temp,name='temp'),
+    path('tempfile/',views.temp,name='temp'),
     path('<int:chai_id>/',views.chai_detail,name='chai_detail'),
     # path('<str:chai_type>/',views.chai_print,name='chai_print'),
     path('chai_store/',views.chai_store_view,name='chai_store'),
